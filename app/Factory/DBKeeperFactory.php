6f03db5ec9652f0275fc87feca3cf543bcb85923
EpickFail <integrator@systech.ru>
2020-06-13 02:14:37 +0500

    first commit

--- app/Factory/DBKeeperFactory.php
+++ app/Factory/DBKeeperFactory.php
@@ -4,7 +4,7 @@ namespace App\Factory;
 
 use App\Keep\DBKeeper;
 
-Class DBKEeperFactory implements KeeperFactory
+Class DBKeeperFactory implements KeeperFactory
 {
     public static function createKeeper()
     {
