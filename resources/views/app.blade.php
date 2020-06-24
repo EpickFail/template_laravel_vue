abbdb8ceeaab8d8feb32fe93ac08e11f45f494a9
EpickFail <integrator@systech.ru>
2020-06-24 14:42:29 +0500

    template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,11 +7,11 @@
         <title>Laravel Form</title>
 
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="{{ secure_asset('public/css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
     </head>
     <body>
         <div id="app"></div>
-        <script src="{{ secure_asset('public/js/app.js') }}"></script>
+        <script src="{{ asset('/js/app.js') }}"></script>
     </body>
 </html>
